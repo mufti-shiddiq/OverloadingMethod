fed25dd1b4b45a87edf5bb1611a781941f418b67
mufti-shiddiq <mufti@students.amikom.ac.id>
2021-04-01 22:13:35 +0700

    inisialisasi project

---
+++ Numbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    public class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            // TODO: lengkapi kode methodnya
+        }
+
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            // TODO: lengkapi kode methodnya
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            // TODO: lengkapi kode methodnya
+        }
+
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            // TODO: lengkapi kode methodnya
+        }
+    }
+}
